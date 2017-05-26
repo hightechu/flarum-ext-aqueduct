e798d4051494b34356c8c1a018162eb9d766d8f7
Daniel Klabbers <daniel@productip.com>
2017-05-26 15:55:59 +0200

    attempting work to implement permission

--- src/Http/Controllers/BoardController.php
+++ src/Http/Controllers/BoardController.php
@@ -2,9 +2,23 @@
 
 namespace Flagrow\Aqueduct\Http\Controllers;
 
+use Flarum\Core\User;
 use Flarum\Forum\Controller\WebAppController;
+use Flarum\Http\Exception\RouteNotFoundException;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 class BoardController extends WebAppController
 {
 
+    protected function getView(Request $request)
+    {
+        /** @var User $actor */
+        $actor = $request->getAttribute('actor');
+
+        if ($actor->cannot('discussion.flagrow.aqueduct.board-access')) {
+            throw new RouteNotFoundException();
+        }
+
+        return parent::getView($request);
+    }
 }
