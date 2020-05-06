96ab9bf4d3191920aab47b2371e85b67c7ded39d
Rio6 <rio6.liu@gmail.com>
2020-05-05 22:36:59 -0700

    Revert useless webpack config change

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,10 +1,3 @@
 const config = require('flarum-webpack-config');
-const merge = require('webpack-merge');
 
-module.exports = merge(config(), {
-    resolve: {
-        alias: {
-            //'yaml': 'node_modules/yaml/dist/'
-        }
-    }
-});
+module.exports = config();
